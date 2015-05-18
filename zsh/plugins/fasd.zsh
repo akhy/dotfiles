e9a7ec2616a87543df8fe3ccdec92377c79ef40f
Akhyar Amarullah <akhyrul@gmail.com>
2015-05-18 16:26:04 +0700

    Add fasd plugin

---
+++ zsh/plugins/fasd.zsh
@@ -0,0 +1,8 @@
+if ! type "fasd" > /dev/null; then
+	curl -L https://github.com/clvv/fasd/raw/master/fasd > $DOTFILES/bin/fasd
+	chmod +x $DOTFILES/bin/fasd
+	echo "FASD has been installed"
+fi
+
+eval "$(fasd --init auto)"
+alias z="fasd_cd -d"
