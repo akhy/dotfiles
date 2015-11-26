5950752c37723375091ce3785be532c985578d8e
Akhyar Amarullah <akhyrul@gmail.com>
2015-11-26 17:20:23 +0700

    Fix rbenv installer script

--- install.d/rbenv.sh
+++ install.d/rbenv.sh
@@ -1,8 +1,9 @@
-if [ ! -s $HOME/.rbenv ]; then
-    rbenv_path=$HOME/.rbenv
+cmd="rbenv"
+rbenv_path=$HOME/.rbenv
+if ! loc="$(type -p "$cmd")" || [ -z "$loc" ] && [ ! -s "$rbenv_path" ]; then
     echo "time to get rbenv"
-    git clone --depth 1 git://github.com/sstephenson/rbenv.git $rbenv_path
-    git clone git://github.com/sstephenson/ruby-build.git $rbenv_path/plugins
+    git clone git://github.com/sstephenson/rbenv.git $rbenv_path
+    git clone git://github.com/sstephenson/ruby-build.git $rbenv_path/plugins/ruby-build
 else
     echo "rbenv already exists - skipping it"
 fi
