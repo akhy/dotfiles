58f6a242bd9f0ff500985941b03d96cc676fc42b
akhy <akhy@chickenzord.com>
2022-01-01 02:19:16 +0700

    remove ansible-based setup

--- playbook/roles/zsh/files/zshrc.d/10_shell.zsh
+++
@@ -1,3 +0,0 @@
-export EDITOR=vim
-export HISTIGNORE="ls:ll:la:cd:cd -:pwd:exit:date:* --help"
-export CLICOLOR=1
--- playbook/roles/zsh/files/zshrc.d/99_cleanup.zsh
+++
@@ -1,1 +0,0 @@
-export PATH=$(echo "$PATH" | awk -v RS=':' -v ORS=":" '!a[$1]++{if (NR > 1) printf ORS; printf $a[$1]}')
